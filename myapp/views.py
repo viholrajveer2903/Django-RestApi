332c3ef7813d694f433249a808f9490382378dec
viholrajveer2903 <viholrajveer2903@gmail.com>
2023-08-24 15:35:23 +0530

    RestAPI

---
+++ myapp/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+
+# Create your views here.
+
+
+#def index(request):
+	#return render(request,'index.html')
+
+class BookList(generics.ListCreateAPIView):
+	queryset=Book.objects.all()
+	serializer_class=BookSerializer
+
+class BookDetail(generics.RetrieveUpdateDestroyAPIView):
+	queryset=Book
+	serializer_class=BookSerializer
